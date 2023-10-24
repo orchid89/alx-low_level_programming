f876a6900775e7b9569c515b55c364dfb8e376a0
orchid89 <orchidoo71@hotmail.com>
2023-10-24 10:39:24 -0700

    ./0-isuper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - prog checks for uppercase character
+ * @c: variable text
+ * Return: always 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'z')
+	{
+		return (1);
+		}
+	else
+	{
+		return (0);
+	}
+}
